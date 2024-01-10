92b5ac9290cb268e99b22b6d2e9dbc90562e65db
Abdulla <abdullamagametov@gmail.com>
2024-01-10 14:02:52 +0600

    error correction

--- serialize.js
+++ serialize.js
@@ -6,7 +6,6 @@ function deserialize(serialized) {
   const chars = serialized.split('');
   return chars.map((char) => char.charCodeAt(0));
 }
-
 // Тесты
 const tests = [
   [1, 2, 3, 4, 5],
@@ -15,9 +14,10 @@ const tests = [
   Array.from({ length: 100 }, () => Math.floor(Math.random() * 300) + 1),
   Array.from({ length: 500 }, () => Math.floor(Math.random() * 300) + 1),
   Array.from({ length: 1000 }, () => Math.floor(Math.random() * 300) + 1),
-  Array.from({ length: 1000 }, (_, index) => (index % 10) + 1), // Все числа 1 знака
-  Array.from({ length: 1000 }, (_, index) => (index % 90) + 10), // Все числа из 2х знаков
-  Array.from({ length: 900 }, (_, index) => (index % 300) + 100), // Каждое число по 3 знака
+  [1, 2, 3, 4, 5, 6, 7, 8, 9], // Все числа 1 знака
+  Array.from({ length: 90 }, (_, index) => index + 10), // Все числа из 2х знаков
+  Array.from({ length: 201 }, (_, index) => index + 100), // Все числа из 3х знаков
+  Array.from({ length: 300 }, (_, index) => [index + 1, index + 1, index + 1]).flat(), // Каждого числа по 3 - всего 900
 ];
 
 tests.forEach((numbers) => {
